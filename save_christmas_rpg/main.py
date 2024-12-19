13bfdaf0af6949e1c60ecee12ac40dad950a2c2d
HaileyQ <thaoqp.hailey@gmail.com>
2024-12-19 15:54:23 -0500

    Update main.py

--- save_christmas_rpg/main.py
+++ save_christmas_rpg/main.py
@@ -1,6 +1,6 @@
 import sys
 import yaml
-from game_builder_crew.crew import GameBuilderCrew
+from crew import GameBuilderCrew
 
 
 def run():
@@ -8,11 +8,11 @@ def run():
     print("## Welcome to the Game Crew")
     print('-------------------------------')
 
-    with open('src/game_builder_crew/config/gamedesign.yaml', 'r', encoding='utf-8') as file:
+    with open('config/gamedesign.yaml', 'r', encoding='utf-8') as file:
         examples = yaml.safe_load(file)
 
     inputs = {
-        'game' :  examples['example3_snake']
+        'game' :  examples['example4_save_christmas']
     }
     game= GameBuilderCrew().crew().kickoff(inputs=inputs)
 
@@ -28,14 +28,21 @@ def train():
     Train the crew for a given number of iterations.
     """
 
-    with open('src/game_builder_crew/config/gamedesign.yaml', 'r', encoding='utf-8') as file:
+    with open('config/gamedesign.yaml', 'r', encoding='utf-8') as file:
         examples = yaml.safe_load(file)
 
     inputs = {
-        'game' : examples['example1_pacman']
+        'game' :  examples['example4_save_christmas']
     }
     try:
         GameBuilderCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
+
+# Main execution point
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "train":
+        train()
+    else:
+        run()
